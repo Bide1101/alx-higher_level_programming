4caa11826d4ec79c5fb37eade86a0ef4fe58e820
Bide1101 <olabiyibisolad@gmail.com>
2023-05-11 14:39:33 -0700

    A  program that prints the number of and the list of its arguments/fixed requirement error

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 import sys
-if len(sys.argv) > 1:
-    if len(sys.argv) == 2:
-        print(len(sys.argv)-1, "argument:")
-        print("1: {:s}".format(sys.argv[1]))
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        if len(sys.argv) == 2:
+            print(len(sys.argv)-1, "argument:")
+            print("1: {:s}".format(sys.argv[1]))
+        else:
+            print(len(sys.argv)-1, "arguments:")
+            for i in sys.argv[1:]:
+                print("{:d}:".format(sys.argv.index(i)), "{:s}".format(i))
     else:
-        print(len(sys.argv)-1, "arguments:")
-        for i in sys.argv[1:]:
-            print("{:d}:".format(sys.argv.index(i)), "{:s}".format(i))
-else:
-    print("0 arguments.")
+        print("0 arguments.")
