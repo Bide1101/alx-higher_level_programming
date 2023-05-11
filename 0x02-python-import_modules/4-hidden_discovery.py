4d701b7494f5307ece46c94ca839c876a1ffd243
Bide1101 <olabiyibisolad@gmail.com>
2023-05-11 15:54:34 -0700

    A program that prints all the names defined by the compiled module hidden_4.pyc

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,18 +1,7 @@
 #!/usr/bin/python3
-import dis
-import marshal
+import hidden_4
 
 if __name__ == "__main__":
-
-    with open('hidden_4.pyc', 'rb') as f:
-        magic = f.read(4)
-        timestamp = f.read(4)
-        code = marshal.load(f)
-
-    instructions = dis.get_instructions(code)
-    for i in instructions:
-        if i.opname == 'LOAD_GLOBAL':
-            name = instr.argval
-            if not name.startswith('__'):
-                print(name)
-
+    for i in dir(hidden_4):
+        if not i.startswith("__"):
+            print(i)
