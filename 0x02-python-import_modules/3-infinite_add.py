5da9b17dbe7e890ddfd7916276ab3a5faffb5773
Bide1101 <olabiyibisolad@gmail.com>
2023-05-11 15:36:28 -0700

    A program that prints the result of the addition of all arguments/fixed pycodestyle Error

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,7 +3,7 @@ import sys
 if __name__ == "__main__":
     sum = int(0)
     if len(sys.argv) == 1:
-        print (sum)
+        print(sum)
     else:
         for i in sys.argv[1:]:
             sum = sum + int(i)
