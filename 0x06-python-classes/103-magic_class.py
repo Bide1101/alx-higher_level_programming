1d425fc1054e8ccd28ce79a797fef360fe4adda2
Bide1101 <olabiyibisolad@gmail.com>
2023-05-30 19:20:10 -0700

    Commenting out

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -7,13 +7,16 @@ import math
 
 
 class MagicClass:
+    """the private init method takes two parameters"""
     def __init__(self, radius):
         if not isinstance(radius, (int, float)):
             raise TypeError('radius must be a number')
         self.__radius = radius
 
+    """definition of the area method, calculates the area of the circle"""
     def area(self):
         return (self.__radius ** 2) * math.pi
 
+    """calculates the circumference of the circle"""
     def circumference(self):
         return 2 * math.pi * self.__radius
