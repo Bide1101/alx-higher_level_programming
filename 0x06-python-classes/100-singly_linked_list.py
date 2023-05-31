4c022ebf6f35b616c55f45d23b9d93819637b4e6
Bide1101 <olabiyibisolad@gmail.com>
2023-05-30 20:01:10 -0700

    A class Node that defines a node of a singly linked list

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -8,26 +8,26 @@ class Node:
         self.data = data
         self.next_node = next_node
 
+    """get the data of the node - takes one parameter"""
     @property
     def data(self):
-        """get the data of the node - takes one parameter"""
         return self.__data
 
+    """set the data of the node"""
     @data.setter
     def data(self, value):
-        """set the data of the node"""
         if not isinstance(value, int):
             raise TypeError("data must be an integer")
         self.__data = value
 
+    """Get the reference to the next Node"""
     @property
     def next_node(self):
-        """Get the reference to the next Node"""
         return self.__next_node
 
+    """ Validate that next_node is a Node object"""
     @next_node.setter
     def next_node(self, value):
-        """ Validate that next_node is a Node object"""
         if value is not None and not isinstance(value, Node):
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
@@ -37,12 +37,12 @@ class Node:
 
 
 class SinglyLinkedList:
+    """init method - Initialize the head of the linked list"""
     def __init__(self):
-        """init method - Initialize the head of the linked list"""
         self.head = None
 
+    """Create a new node with the given value"""
     def sorted_insert(self, value):
-        """Create a new node with the given value"""
         new_node = Node(value)
 
         if self.head is None:
