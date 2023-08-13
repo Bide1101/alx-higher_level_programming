03f52dcf01d6af16de6fca09c49fac7887a6a7cc
Bide1101 <olabiyibisolad@gmail.com>
2023-08-13 13:06:58 -0700

    correction of code error

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        count = 0
-        for element in my_list:
-            print(element, end='')
+
+    count = 0
+    for e in range(x):
+        try:
+            print(my_list[e], end='')
             count += 1
-            if count == x:
-                break
-    except TypeError:
-        print("Error: Invalid input!")
-    finally:
-        print()
-        return count
+        except IndexError:
+            break
+    print()
+    return count
