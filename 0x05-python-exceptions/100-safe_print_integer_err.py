49509df91f3f70dcb7fbcc2224d58e22ffc710a1
Bide1101 <olabiyibisolad@gmail.com>
2023-08-13 14:29:34 -0700

    A function that prints an integer: returns True or False

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -7,6 +7,6 @@ def safe_print_integer_err(value):
         formatted_value = "{:d}".format(value)
         print(formatted_value)
         return True
-    except ValueError:
-        print("Exception:", value, file=sys.stderr)
+    except (ValueError, TypeError):
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return False
