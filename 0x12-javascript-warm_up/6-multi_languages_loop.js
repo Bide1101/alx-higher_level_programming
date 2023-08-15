bd94a650905ddb57d40e58101e0625f90edc309a
Bide1101 <olabiyibisolad@gmail.com>
2023-08-14 21:09:41 -0700

    This prints 3 lines by using an array of string and a loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/* This prints 3 lines by using an array of string and a loop */
+
+const myArr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let i;
+
+for (i = 0; i < myArr.length; i++) {
+  console.log(myArr[i]);
+}
