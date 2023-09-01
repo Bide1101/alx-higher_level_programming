ba9d0bbce201935899e5feae169f0ba2b1ac54a6
Bide1101 <olabiyibisolad@gmail.com>
2023-08-31 22:26:59 -0700

    fixed error

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -8,8 +8,18 @@ def find_peak(list_of_integers):
     """
     This function is used to find the 'peak', using the lowest complexity
     """
-    try:
-        peakNum = max(list_of_integers)
-        return peakNum
-    except ValueError:
+
+    if not list_of_integers:
         return None
+
+    left, right = 0, len(list_of_integers) - 1
+
+    while left < right:
+        mid = (left + right) // 2
+
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            left = mid + 1
+        else:
+            right = mid
+
+    return list_of_integers[left]
