f2d4fe4694b652e24c7d752bf6df8a5eafca822a
Bide1101 <olabiyibisolad@gmail.com>
2023-11-14 02:38:15 -0800

    JavaScript is amazing

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,2 +1,5 @@
 #!/usr/bin/node
-console.log(process.argv)
+const args = process.argv;
+const message = (args.length === 3) ? 'Argument found' : (args.length > 3) ? 'Arguments found' : 'No argument';
+
+console.log(message);
---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+const args = process.argv;
+const message = args[2] ? args[2] : 'No argument';
+console.log(message);
