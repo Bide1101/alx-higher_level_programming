d32e0c1593302331b4d2b49e2bedc61b46d37166
Bide1101 <olabiyibisolad@gmail.com>
2023-11-15 11:42:40 -0800

    This script prints 3 lines using array of string and a loop

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 const args = process.argv;
-const message = (args[2] % 10) ? ('My number: '+(args[2])) : 'Not a number';
+const message = (args[2] % 10) ? ('My number: ' + (args[2])) : 'Not a number';
 console.log(message);
---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+const arrString = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let a = 0; a < arrString.length; a++) {
+  console.log(arrString[a]);
+}
